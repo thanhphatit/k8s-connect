5113e7e72eaab989180fa63a6675ff0e9515ac58
root <thanhphatit95@gmail.com>
2024-04-23 08:34:12 +0000

    Update time: 20240423-083409

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# k8s-connect
-A simple tools to connect shell nodes in kubernetes
+# K8S-CONNECT
+
+This is simple tools created by bash shell and connect to nodes in kubernetes
+
+NOTE:
+
+    [+] Your machine need have bash and OS linux (Ubuntu is good)
+    [+] The tools run on x86-64 and arm64 of linux (it's not support for macOS)
+
+## Install
+
+### Terminal
+
+To install on terminal.
+
+###### CURL: 
+
+```
+sudo curl -o- https://raw.githubusercontent.com/thanhphatit/k8s-connect/main/install.sh | /bin/bash
+```
+
+###### WGET: 
+
+```
+sudo wget -q https://raw.githubusercontent.com/thanhphatit/k8s-connect/main/install.sh -O - | /bin/bash
+```
+
+### Help
+
+You can use `k8s-connect -h` to show help user
+
+
+
---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+## Author: Dang Thanh Phat
+## Email: thanhphatit95@gmail.com
+## Web/blogs: www.itblognote.com
+##      sudo curl -o- https://raw.githubusercontent.com/thanhphatit/k8s-connect/main/install.sh | /bin/bash
+##      sudo wget -q https://raw.githubusercontent.com/thanhphatit/k8s-connect/main/install.sh -O - | /bin/bash
+
+###### GLOBAL VARIABLES
+BIN_NAME="k8s-connect"
+REPO_OWNER="thanhphatit"
+GIT_BRANCH="main"
+GIT_PATH_DOWN_FILE="${BIN_NAME}.deb"
+REPO_RELEASE_NAME="${BIN_NAME}"
+TMP_INSTALL_ROOT=$(mktemp -dt ${BIN_NAME}-install-XXXXXX)
+TMP_INSTALL_FILE="${TMP_INSTALL_ROOT}/${GIT_PATH_DOWN_FILE}"
+
+###### FUNCTIONS
+function download_file(){
+    local _DOWNLOAD_URL="https://raw.githubusercontent.com/${REPO_OWNER}/${REPO_RELEASE_NAME}/${GIT_BRANCH}/${GIT_PATH_DOWN_FILE}"
+
+    if [[ $(command -v curl) ]];then
+        curl -SsL "${_DOWNLOAD_URL}" -o "${TMP_INSTALL_FILE}"
+    else
+        wget -q -O "${TMP_INSTALL_FILE}" "${_DOWNLOAD_URL}"
+    fi    
+}
+
+###### MAIN
+function main(){
+    download_file
+
+    if [[ -f ${TMP_INSTALL_FILE} ]];then
+        chmod 755 ${TMP_INSTALL_FILE}
+        dpkg -i ${TMP_INSTALL_FILE}
+    fi
+
+    if [[ -d ${TMP_INSTALL_ROOT} ]];then
+        rm -rf ${TMP_INSTALL_ROOT}
+    fi
+}
+
+###### START
+main "${@}"
+
+exit 0
